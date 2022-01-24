00330ab4f72bd0aca9d9d8731cb93dcf90bf357f
aprdc2anil <aprdc2anil@gmail.com>
2022-01-24 11:47:11 +0530

    Initial commit

---
+++ src/com/aprdc2anil/javapractice/Main.java
@@ -0,0 +1,8 @@
+package com.aprdc2anil.javapractice;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world");
+    }
+}
